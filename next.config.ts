8bf65d5367ef0c5620657aa9b2988150e5ec1978
Mohammad Zgoul <mohammad@zg0ul.com>
2025-12-21 15:18:13 +0300

    Remove experimental SVG loader configuration and eslint ignore setting from Next.js config for cleaner setup.

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     config.resolve.alias.canvas = false;
@@ -56,9 +46,6 @@ const nextConfig: NextConfig = {
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   output: "standalone",
   reactStrictMode: true,
